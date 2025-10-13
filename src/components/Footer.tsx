414a6c84a99dcac37aafc9df8a809c39757cabf2
Adilkhan Ersin <ersinadilhan.51@gmail.com>
2025-10-13 23:10:37 +0500

    Some minor change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,11 +19,11 @@ export default function Footer() {
         </ul>
         <h3 className="font-bold text-foreground text-sm sm:text-base mb-1">При обнаружении списывания — <span className="text-primary">ОТЧИСЛЕНИЕ.</span></h3>
       </div>
-      <div className='max-w-4xl mx-auto flex justify-center items-center gap-x-4'>
+      {/* <div className='max-w-4xl mx-auto flex justify-center items-center gap-x-4'>
         <a href="https://www.instagram.com/adilikecious/" target="_blank" rel="noopener noreferrer"><Instagram className="w-10 h-8 text-accent-foreground hover:scale-110 transition-all" /></a>
         <a href="https://t.me/safemys" target="_blank" rel="noopener noreferrer"><Send className="w-10 h-8 text-accent-foreground hover:scale-110 transition-all" /></a>
         <a href="mailto:253209@astanait.edu.kz" target="_blank" rel="noopener noreferrer"><Mail className="w-10 h-8 text-accent-foreground hover:scale-110 transition-all" /></a>
-      </div>
+      </div> */}
       <p className="text-center text-foreground text-sm mt-4">
         &copy; 2025. All rights couldn't be reserved. 
       </p>
